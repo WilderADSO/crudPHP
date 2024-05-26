cef47240c886d71826abb5dbfee2cd27ff0c4691
wilderADSO <winogaos2019@gmail.com>
2024-05-26 13:27:48 -0500

    _MVC: Funcion Adicionar Producto BD

--- controladores/ProductoControlador.php
+++ controladores/ProductoControlador.php
@@ -15,5 +15,28 @@ class ProductoControlador {
         include './vistas/productos_view.php';
     }
 
+   // Controlador para Mostrar formulario
+    public function mostrarFormularioAgregarProducto(): void {
+        include './Vistas/modaladdproducto.php';
+    }
+
+// Controlador para Adicionar Productos
+    public function agregarProducto(): void {
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $nombre = $_POST['nombre'];
+            $stock = $_POST['stock'];
+            $precio = $_POST['precio'];
+            $exito = $this->modeloProducto->agregarProducto($nombre, $stock, $precio);
+            if ($exito) {
+                header("Location: index.php");
+                exit();
+            } else {
+                exit("Error al agregar el producto");
+            }
+        }
+    }
+
+
+
 }
 ?>
